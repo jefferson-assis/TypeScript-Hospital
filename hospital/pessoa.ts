9b0e414b70f6dac8c47dca7d95c50724abb84eaa
Jefferson de Assis <jeffersondeassis13022000@gmail.com>
2020-03-07 01:37:13 +0000

    adicionando GET

--- hospital/pessoa.ts
+++ hospital/pessoa.ts
@@ -1,11 +1,20 @@
 namespace empresa{
     export class Pessoa{
+        //informação da classe
         private nome:string;
         private cpf:string;
 
         constructor(nome:string, cpf:string){
             this.cpf = cpf;
             this.nome = nome;
+        }
+
+        getCpf():string{
+            return this.cpf;
+        }
+
+        getNome():string{
+            return this.nome;
         }
     }
 }
